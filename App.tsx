595912ba4ee3d2152c7101a2f5e9a36e1f5681e6
ZIHAD22 <mdzihad2244@gmail.com>
2023-05-23 00:34:01 +0600

    frist

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , SafeAreaView, Platform , StatusBar} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Hello
+      </Text>
     </View>
   );
 }
 
+console.log(StatusBar.currentHeight)
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor:"rgb(86 131 205)",
+    paddingHorizontal:5,
   },
 });
